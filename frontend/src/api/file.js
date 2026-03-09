61e18569e5df21c6cb3fa13ae730889481ab7643
MingLoong <52956168+MingLoong@users.noreply.github.com>
2026-03-09 20:42:34 +0800

    Update file.js

--- frontend/src/api/file.js
+++ frontend/src/api/file.js
@@ -2,3 +2,25 @@ import request from '@/utils/request'
 export function fileList(path, sshInfo) {
     return request.get(`/file/list?path=${path}&sshInfo=${sshInfo}`)
 }
+
+export function fileDelete(path, sshInfo) {
+    const data = new URLSearchParams()
+    data.append('path', path)
+    data.append('sshInfo', sshInfo)
+    return request.post('/file/delete', data)
+}
+
+export function fileCopy(path, sshInfo) {
+    const data = new URLSearchParams()
+    data.append('path', path)
+    data.append('sshInfo', sshInfo)
+    return request.post('/file/copy', data)
+}
+
+export function filePaste(srcPath, dstPath, sshInfo) {
+    const data = new URLSearchParams()
+    data.append('srcPath', srcPath)
+    data.append('dstPath', dstPath)
+    data.append('sshInfo', sshInfo)
+    return request.post('/file/paste', data)
+}
